5bd7350d5b30cd89ab249c54cd59a0c02bac1180
Łukasz Kuczek <lkuczek@wp.pl>
2017-10-11 19:30:23 +0200

    Ready

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 (function($) {
     $(document).ready(function() {
 
+        var button = $("img.menu-icon"),
+            menu = $("#hamburger-menu");
+
+        button.on('click', function(e){
+            console.log(e.currentTarget);
+            menu.toggleClass("visible");
+        });
     });
+
+    var  h1 = $(".content .container .row h1");
+    h1.on("showContent", function() {
+        $(this).addClass("visible");
+    });
+    $(window).on("load", function() {
+        $(".content .container .row h1").trigger("showContent");
+    });
+
 })(jQuery);
